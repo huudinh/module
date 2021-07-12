e9c348ae77d38cd593903aee0e4d2df02d5d8105
MinhPhuong195 <phuongmp99@gmail.com>
2021-07-12 16:04:23 +0700

    fix

---
+++ Home/about_8_0_0/js/about_8_0_0.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName('accordions-title')[0].addEventListener('click',()=>{
+    document.getElementsByClassName('accordions-content')[0].classList.toggle("accordions-content_active");
+})
