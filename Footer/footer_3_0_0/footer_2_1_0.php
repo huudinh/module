1490e4361abf53c612817996e96bbbd123ad6b95
MinhPhuong195 <phuongmp99@gmail.com>
2022-02-11 14:47:38 +0700

    add footer

---
+++ Footer/footer_3_0_0/footer_2_1_0.php
@@ -0,0 +1,98 @@
+<?php
+    if($check == 0){
+        include(locate_template("Module_v2/Footer/footer_2_1_0/css/footer_2_1_0_css.php"));
+	}
+?>
+<footer class="footer_2_1_0">
+    <!-- <div class="refuse ">
+        <div class="container">
+            Hiệu quả phụ thuộc vào cơ địa mỗi người (*)
+        </div> -->
+        <!-- <a href="javascript:void(0)" class="scrolltop"><img src="<?php echo ''.$path.''?>/images/scroll-top.png" class="aligncenter"> <span>Về đầu trang!</span></a> -->
+    <!-- </div> -->
+    <div class="footer_2_1_0__container container">
+        <div class="footer_2_1_0__box">
+            <div class="footer_2_1_0__box1">
+                <div class="footer_2_1_0__box1-logo">
+                    <a href="https://nhakhoaparis.vn/">
+                        <picture>
+                            <source media="(min-width:600px)" srcset="<?php echo ''.$path.''?>/images/logo.png">
+                            <img width="306" height="112" class="lazy" data-src="<?php echo ''.$path.''?>/images/logo.png" alt="Nha Khoa Paris">
+                        </picture>
+                    </a>
+                </div>
+                <a href="http://online.gov.vn/Home/WebDetails/67689" class="img_bct" rel="nofollow" target="_blank">
+                    <img width="153" height="58"  alt="Thông báo bộ công thương" title="" class="lazy" data-src="<?php echo ''.$path.''?>/images/logoSaleNoti.png"/>
+                </a>
+            </div>
+            <div class="footer_2_1_0__box2">
+                <div class="footer_2_1_0__box2__item item1">
+                    <div class="footer_2_1_0__box2__ct">
+                        <div class="footer_2_1_0__box2-title">
+                            <?php
+                                if( is_front_page() || is_home()):
+                            ?>
+                            <p>hệ thống chuỗi nha khoa tiêu chuẩn pháp</p>
+                            <?php else: ?>
+                                <p>hệ thống chuỗi nha khoa tiêu chuẩn pháp</p>
+                            <?php 
+                                endif;
+                            ?>
+
+                            <p><b>Bác sĩ tư vấn 24/7</b> 0943.776.699</p>
+                        </div>
+                        <div class="footer_2_1_0__box2-Social">
+                            <a class="boxtop__call-2" href="tel:19006900">
+                            1900.6900
+                        </a>
+                            <a href="#" target="_blank" rel="nofollow" class="footer_2_1_0__box2-Social-facebook"></a>
+                            <a href="#" target="_blank" rel="nofollow" class="footer_2_1_0__box2-Social-youtube"></a>
+                            <!-- <a href="https://www.facebook.com/NhaKhoaParis/" target="_blank" rel="nofollow" class="footer_2_1_0__box2-Social-facebook"></a>
+                            <a href="https://www.youtube.com/channel/UCHMf1HRdqpBRqP6p-qZyGog" target="_blank" rel="nofollow" class="footer_2_1_0__box2-Social-youtube"></a> -->
+                            <!-- <a href="#" target="_blank" rel="nofollow" class="footer_2_1_0__box2-Social-zalo"></a>
+                            <a href="#" target="_blank" rel="nofollow" class="footer_2_1_0__box2-Social-insta"></a> -->
+                        </div>
+                    </div>
+                    <div class="footer_2_1_0__box2__pic">
+                        <!-- <a href=" https://www.facebook.com/NhaKhoaParis/" target="_blank" rel="nofollow"> -->
+                        <a href="#" target="_blank" rel="nofollow">
+                            <img width="252" height="96" class="lazy" data-src="<?php echo ''.$path.''?>/images/fb.png" alt="Facebook Nha Khoa Paris">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="footer_2_1_0__box2__item">
+                    <div class="footer_2_1_0__box2__DoiTac dt2">
+                        <ul>
+                            <li class="fra"><b>Đối tác Pháp:</b> Cabinet Dentaire Philippe Tarot - 9 Rue Rempart de l'Oulle, 84000 Avignon, France</li>
+                            <li class="vn"><b>Cơ sở Việt Nam:</b> Hà Nội - Bắc Ninh - Hải Phòng - Thanh Hóa - Vinh - Đà Nẵng - TP.HCM - Quảng Ninh - &nbsp; <br> Bình Dương</li>
+                        </ul>
+                    </div>
+                    <div class="footer_2_1_0__box2__protect">
+                        <a href="//www.dmca.com/Protection/Status.aspx?ID=b192a6ce-8f99-437e-8933-163c70a4523c" title="DMCA.com Protection Status" rel="nofollow" >
+                            <img width="135" height="28" class="lazy" data-src="<?php echo ''.$path.''?>/images/protect.jpg" alt="Chứng chỉ DMCA">
+                        </a>
+                        <!-- <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script> -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="footer_2_1_0_sub">
+            CÔNG TY TNHH NHA KHOA PARIS<br>
+            Giấy chứng nhận đăng ký doanh nghiệp số: 0107473432 do Phòng Đăng ký kinh doanh - Sở Kế hoạch và Đầu tư TP. Hà Nội cấp ngày 14/6/2016<br>
+            Giấy phép hoạt động khám bệnh, chữa bệnh số: 3028/SYT - GPHĐ do Sở Y tế TP. Hà Nội cấp ngày 14/10/2016<br>
+            Địa chỉ: Số 110 - 112 Bà Triệu, Hoàn Kiếm, Hà Nội<br>
+            <a href="https://nhakhoaparis.vn/chinh-sach-bao-mat-thong-tin-khach-hang-tai-nha-khoa-paris.html" target="_blank" rel="nofollow">Chính sách bảo mật</a>
+        </div>
+    </div>
+    <div class="refuse">
+        <div class="container">
+            Hiệu quả phụ thuộc vào cơ địa mỗi người (*)
+        </div>
+        <!-- <a href="javascript:void(0)" class="scrolltop"><img src="<?php echo ''.$path.''?>/images/scroll-top.png" class="aligncenter"> <span>Về đầu trang!</span></a> -->
+    </div>
+
+</footer>
